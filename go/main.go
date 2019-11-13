febc871a25e94ee753995def9d9834ed292db021
wasupandceacar <wasupandceacar@gmail.com>
2019-11-12 23:39:16 -0500

    更新程序内说明

--- go/main.go
+++ go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"github.com/MakeNowJust/hotkey"
 	"math"
@@ -15,18 +16,28 @@ func main(){
 	c := make(chan os.Signal)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
+	fmt.Println("Made by wasupandceacar")
+	fmt.Println("=======================================================")
+	fmt.Println("Guide:")
+	fmt.Println("Press ] to increase gamma (max value: 1)")
+	fmt.Println("Press [ to decrease gamma (min value: -1)")
+	fmt.Println("Press Ctrl+C to exit, and gamma will return to standard (value: 0)")
+	fmt.Println("Don't click close button to exit, gamma will not return back")
+	fmt.Println("If you accidentally do so, just restart and it will set gamma to standard initially")
+	fmt.Println("=======================================================")
+
 	getGammaFunc()
 	getHDC()
 	g, gamma := initGamma()
 
 	hkey.Register(hotkey.None, 0xDD, func() {
-		log.Println("gamma up")
 		gamma = math.Max(-1, gamma - 0.05)
+		log.Println("gamma up, current gamma:", fmt.Sprintf("%.2f", -gamma))
 		adjustGamma(g, gamma)
 	})
 	hkey.Register(hotkey.None, 0xDB, func() {
-		log.Println("gamma down")
 		gamma = math.Min(1, gamma + 0.05)
+		log.Println("gamma down, current gamma:", fmt.Sprintf("%.2f", -gamma))
 		adjustGamma(g, gamma)
 	})
 
